d26910a07187e72815c3990f93672a3d4351a570
Avishek Jana <avishek@geogo.in>
2022-01-16 12:06:09 +0530

    DB Credentials added in ENV vars

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // Hold application secret and configurations
 module.exports = {	
-	mongoURI: "mongodb+srv://geogofswi:geogo_fswi@cluster0.92wt8.mongodb.net/fswiSwiggyClone?retryWrites=true&w=majority"
+	mongoURI: process.env.MONGO_CONNECT
 };
 
 // module.exports = {	
